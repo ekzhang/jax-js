8b4e38033cdf9d86b551d1401146e3cef8d2a35c
Eric Zhang <ekzhang1@gmail.com>
2025-02-03 20:37:07 -0500

    Set up more boilerplate for browser testing and TS

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2,6 +2,17 @@ import { x } from "jax-js";
 
 import { expect, test } from "vitest";
 
-test("works", () => {
+test("x is 3", () => {
   expect(x).toBe(3);
+});
+
+test("has webgpu", async () => {
+  const adapter = await navigator.gpu?.requestAdapter();
+  const device = await adapter?.requestDevice();
+  if (!adapter || !device) {
+    throw new Error("No adapter or device");
+  }
+  console.log(device.adapterInfo.architecture);
+  console.log(device.adapterInfo.vendor);
+  console.log(adapter.limits.maxVertexBufferArrayStride);
 });
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+/// <reference types="@vitest/browser/providers/playwright" />
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    browser: {
+      enabled: true,
+      provider: "playwright",
+      // https://vitest.dev/guide/browser/playwright
+      instances: [{ browser: "chromium" }],
+    },
+  },
+});
