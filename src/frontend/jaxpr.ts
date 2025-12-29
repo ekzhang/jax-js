c999c3e1f1d42a5f02ab04958a6791e8458bbe91
Eric Zhang <ekzhang1@gmail.com>
2025-12-28 21:11:30 -0600

    Re-add gather primitive

--- src/frontend/jaxpr.ts
+++ src/frontend/jaxpr.ts
@@ -835,6 +835,31 @@ export const abstractEvalRules: { [P in Primitive]: AbstractEvalRule<P> } = {
     }
     return [new ShapedArray(shape, DType.Uint32, false)];
   },
+  [Primitive.Gather]([x, ...indices], { axis, outDim }) {
+    for (const a of indices)
+      if (a.dtype !== DType.Int32 && a.dtype !== DType.Uint32)
+        throw new TypeError(
+          `Gather indices must be Int32 or Uint32, got ${a.dtype}`,
+        );
+    if (axis.length !== indices.length)
+      throw new TypeError(`Gather: ${axis} axes but ${indices.length} indices`);
+    if (indices.length === 0)
+      throw new TypeError("Gather must have 1+ indices with same shape");
+    if (axis.some((a) => a < 0 || a >= x.shape.length))
+      throw new TypeError("Gather axis out of bounds");
+    if (outDim < 0 || outDim > x.shape.length - axis.length)
+      throw new TypeError("Gather outDim out of bounds");
+    const axisSet = new Set(axis);
+    if (axisSet.size !== axis.length)
+      throw new TypeError("Gather axes are not unique");
+    const gatherShape = indices.reduce<number[]>(
+      (shape, a) => generalBroadcast(shape, a.shape),
+      [],
+    );
+    const newShape = x.shape.filter((_, i) => !axisSet.has(i));
+    newShape.splice(outDim, 0, ...gatherShape);
+    return [new ShapedArray(newShape, x.dtype, x.weakType)];
+  },
   [Primitive.Transpose]([x], { perm }) {
     return [
       new ShapedArray(
