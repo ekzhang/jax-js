a8cd8da3a46cde5cec78fb19ce907f49983c225c
Eric Zhang <ekzhang1@gmail.com>
2026-01-02 22:19:02 -0800

    Add `split()` function

--- src/library/numpy.ts
+++ src/library/numpy.ts
@@ -414,6 +414,62 @@ export function flip(x: ArrayLike, axis: core.Axis = null): Array {
 }
 
 /**
+ * Split an array into multiple sub-arrays along an axis.
+ *
+ * @param a - The input array to split.
+ * @param indicesOrSections - If an integer, it indicates the number of equal
+ * sections to create along the specified axis. If a list of integers, it
+ * specifies the indices at which to split the array.
+ * @param axis - The axis along which to split the array. Default is 0.
+ */
+export function split(
+  a: ArrayLike,
+  indicesOrSections: number | number[],
+  axis: number = 0,
+): Array[] {
+  a = fudgeArray(a);
+  axis = checkAxis(axis, a.ndim);
+  const size = a.shape[axis];
+  let sizes: number[];
+  if (typeof indicesOrSections === "number") {
+    if (size % indicesOrSections !== 0) {
+      throw new Error(
+        `Array of size ${size} cannot be split into ${indicesOrSections} equal parts`,
+      );
+    }
+    const partSize = size / indicesOrSections;
+    sizes = rep(indicesOrSections, partSize);
+  } else {
+    const indices = indicesOrSections;
+    sizes = [indices[0]];
+    for (let i = 1; i < indices.length; i++)
+      sizes.push(indices[i] - indices[i - 1]);
+    sizes.push(size - indices[indices.length - 1]);
+  }
+  // Split in groups of up to 8 outputs, as the transpose rule turns into a
+  // Concatenate primitive that has limited input arguments.
+  const results: Array[] = [];
+  for (let i = 0; i < sizes.length; i += 7) {
+    if (i === sizes.length) {
+      results.push(a);
+      break;
+    } else if (i + 8 >= sizes.length) {
+      results.push(...(core.split(a, axis, sizes.slice(i)) as Array[]));
+      break;
+    } else {
+      const groupSizes = [
+        ...sizes.slice(i, i + 7),
+        sizes.slice(i + 7).reduce((x, y) => x + y, 0),
+      ];
+      const outs = core.split(a, axis, groupSizes) as Array[];
+      results.push(...outs.slice(0, -1));
+      a = outs[outs.length - 1];
+    }
+  }
+  return results;
+}
+
+/**
  * Join a sequence of arrays along an existing axis.
  *
  * The arrays must have the same shape, except in the dimension corresponding to
@@ -421,7 +477,7 @@ export function flip(x: ArrayLike, axis: core.Axis = null): Array {
  *
  * No scalars can be passed to this function, as the axis is then ambiguous.
  */
-export function concatenate(xs: Array[], axis: number = 0) {
+export function concatenate(xs: Array[], axis: number = 0): Array {
   if (xs.length === 0)
     throw new Error("Need at least one array to concatenate");
   const shapes = xs.map(shape);
@@ -455,7 +511,7 @@ export function concatenate(xs: Array[], axis: number = 0) {
  *
  * All shapes must have the same shape.
  */
-export function stack(xs: ArrayLike[], axis: number = 0) {
+export function stack(xs: ArrayLike[], axis: number = 0): Array {
   if (xs.length === 0) {
     throw new Error("Need at least one array to stack");
   }
