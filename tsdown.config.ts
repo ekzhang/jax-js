c4411459cf85f17cd9509574440f824cb0dfcb18
Eric Zhang <ekzhang1@gmail.com>
2026-06-20 23:55:40 -0400

    bump tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,16 +1,18 @@
 import { readdir } from "node:fs/promises";
 
-import { defineConfig, type Options } from "tsdown";
+import { defineConfig, type UserConfig } from "tsdown";
 
 const watchMode = process.env.TSDOWN_WATCH_MODE === "1";
 
 // Common options for all packages.
-const opts: Options = {
-  external: (id: string) => {
-    // Externalize all imports by default, except for runtime helpers generated
-    // by the compiler / bundler toolchain.
-    if (id.startsWith("@oxc-project/runtime")) return false;
-    return Boolean(id.match(/^[^./]/));
+const opts: UserConfig = {
+  deps: {
+    neverBundle: (id: string) => {
+      // Externalize all imports by default, except for runtime helpers generated
+      // by the compiler / bundler toolchain.
+      if (id.startsWith("@oxc-project/runtime")) return false;
+      return Boolean(id.match(/^[^./]/));
+    },
   },
   format: ["cjs", "esm"],
   platform: "browser",
@@ -35,4 +37,4 @@ export default defineConfig([
     cwd: `packages/${pkg}`,
     watch: watchMode && `packages/${pkg}/src`, // Unaffected by cwd.
   })),
-] as Options[]);
+] as UserConfig[]);
